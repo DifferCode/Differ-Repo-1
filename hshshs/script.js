dabe98d1615d396d648c535d24dd3b72c4eaaab2
d <d>
2023-03-25 21:37:28 +0800

    s

---
+++ hshshs/script.js
@@ -0,0 +1,24 @@
+function display() {
+    alert("Hello pu❤️...");
+    alert("Atin kumu pu sabyann😅");
+    alert("Ahmmm balamu pu megbayu kapu HAHAHA kase pu balamu medyo milako ka panga sweet HAHAHAH");
+    alert("Isip kupu baka pu sasawa naka pu😌");
+    alert("Ahmmm pasensya na kayu pu kasi pane kupu alang a topic keka...");
+    alert("Sorry pu ini mupu kasi yaku eh");
+    alert("Kaya pen pu kakarug ku mag gf pu dahil keng pota ma bored lamu kaku HAHAHAH");
+    alert("Pero ita pu dintang kapu tapus ita tinry kupu male mu naman😅");
+    alert("HAHAHAH kaluguran daka pu sobraaa♥️");
+    alert("Pero pu kung sasawa naka pu kanaku sabyan mumu pu😙");
+    alert("Okaya pag atin kapu buring aliwa sabyan mumu pu");
+    alert("Ali kupu kasi buri patse mangapilitan naka mu pu HAHAHA");
+    alert("Ali kapu maiinis kanaku, sasabyan kumu pu hehehe😌");
+    alert("Basta yaku pu anjang palagi pung makanta ali daka lakwan❤️");
+    alert("Meg pramis kupu keka na ika mupu😙");
+    alert("Ott tuluy balamu aiisip ku ali tamu maglambat😭😭");
+    alert("Pasensya naka pu nanu ngan aiisip ku HAHAHAHA");
+    alert("Ita pu basta paka tandanan mupu ika mu lagi ing love ku<3");
+    alert("I love youuuu puuu❤️❤️");
+    alert("Ita mupu buri ku sabyan hehehe");
+}
+
+display();
