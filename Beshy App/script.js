37557743be05c4f3514c4ddeb2e2d67612a00d52
x <x>
2023-07-04 12:45:35 +0800

    x

---
+++ Beshy App/script.js
@@ -0,0 +1,20 @@
+let input = document.getElementById("input");
+let output = document.getElementById("output");
+let copyButton = document.getElementById("copy-button");
+let clearButton = document.getElementById("clear-button");
+
+input.addEventListener('input', () => {
+    let text = input.value.replaceAll(" ", "🤸");
+    output.textContent = text;
+});
+
+copyButton.addEventListener('click', () => {
+    output.select();
+    document.execCommand('copy');
+    alert('Text was copied to your clipboard');
+});
+
+clearButton.addEventListener('click', () => {
+    input.value = "";
+    output.value = "";
+});
